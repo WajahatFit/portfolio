20ecf134b5a84a103ddeaba0af385e49b87f1f56
Syed Wajahat Ali <wajahat.klk25.com>
2023-01-09 21:50:11 +0100

    project component updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,20 +3,18 @@ import orange from "../images/orange store.png";
 
 export default function Projects() {
   return (
-    <div id="projects" className="pt-14">
+    <div id="projects" className="h-full rounded-xl mt-14 pb-4">
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-xl text-center">My Recents Projects</h1>
-        <div className="w-11/12 h-72 bg-white border border-grey-500">
-          <div>
-            <img className="" src={orange} alt="orange-store"></img>
-          </div>
-          <div>
-            <h2 className="p-4">
-              This is a final project developed in Ironhack. It's a e-commerce
-              store which sells tech products.{" "}
-            </h2>
-          </div>
+
+        <img className="object-cover" src={orange} alt="orange-store" />
+        <div>
+          <h2 className="text-md text-center  ">
+            This is a final project developed in Ironhack. It's a e-commerce
+            store which sells tech products.{" "}
+          </h2>
         </div>
+
         <div className="flex w-full justify-evenly">
           <h1 className="text-xl">
             <a href="https://github.com/WajahatFit/Apple-Clone-Front-End">
